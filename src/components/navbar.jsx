1797a9118dc83864f0e287a73e05a13f9723b234
Aditya-12005 <adityahada49@gmail.com>
2025-07-19 12:55:25 +0530

    changed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css';
+import './navbar.css';
 
 const Navbar = () => {
   const [showDropdown, setShowDropdown] = useState(false);
